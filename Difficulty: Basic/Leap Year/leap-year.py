480af072a1ce080557b0d7e186fee26a6d81e412
Vinit Kumar <98692845+vinit-kumar1977@users.noreply.github.com>
2026-08-02 03:17:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Leap Year/leap-year.py
@@ -0,0 +1,11 @@
+class Solution:
+    def checkYear (self, n):
+        # code here
+        if n % 400 == 0:
+            return True
+        elif n % 100 == 0:
+            return False
+        elif n % 4 == 0:
+            return True
+        else:
+            return False
