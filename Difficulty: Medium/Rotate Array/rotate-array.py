72faddd1e45cbeda8a66b7f375ccf18b7e54c0c5
Vinit Kumar <98692845+vinit-kumar1977@users.noreply.github.com>
2026-08-02 17:39:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rotate Array/rotate-array.py
+++ Difficulty: Medium/Rotate Array/rotate-array.py
@@ -5,12 +5,10 @@ class Solution:
         d = d % n
         def reverseArr(startIdx,endIdx):
             while endIdx > startIdx:
-                temp = arr[startIdx]
-                arr[startIdx] = arr[endIdx]
-                arr[endIdx] = temp
-                startIdx += 1
-                endIdx-=1
-            return arr
+                arr[startIdx],arr[endIdx] = arr[endIdx],arr[startIdx]
+                startIdx+=1
+                endIdx -=1
+            # return arr
         reverseArr(0,d-1)
         reverseArr(d,n-1)
         reverseArr(0,n-1)
